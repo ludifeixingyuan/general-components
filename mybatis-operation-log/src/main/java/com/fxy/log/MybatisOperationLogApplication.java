f3569ada446b1e325a44c76d3593a7b6fc358cff
王宗滨 <97536151@qq.com>
2021-08-28 20:03:16 +0800

    first commit

---
+++ mybatis-operation-log/src/main/java/com/fxy/log/MybatisOperationLogApplication.java
@@ -0,0 +1,13 @@
+package com.fxy.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisOperationLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisOperationLogApplication.class, args);
+    }
+
+}
