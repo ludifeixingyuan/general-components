2b8a7e9da57e8de087908a17d6e288db8fa345a3
wangzongbin <wangzongbin@wlmkids.com>
2021-10-18 23:06:32 +0800

    [新增功能](master): fix name

--- operation-log/src/main/java/com/pilot/log/annotion/OperationLog.java
+++ operation-log/src/main/java/com/pilot/log/annotion/OperationLog.java
@@ -15,7 +15,7 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface OPChangeLog {
+public @interface OperationLog {
 
     /**
      * 表名
